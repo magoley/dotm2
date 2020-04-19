e0f73278d3d401e89970e0796288e45f11a24862
magoley <63185152+magoley@users.noreply.github.com>
2020-04-19 20:03:14 +0200

    Go Live - Yeah! mail.php war

--- send_email.php
+++ send_email.php
@@ -1,6 +1,6 @@
 <?php
 //send_email.php
-$email_from = "absender@domain.de";   //Absender falls keiner angegeben wurde
+$email_from = "absender@agile-podcast.de";   //Absender falls keiner angegeben wurde
 $sendermail_antwort = true;      //E-Mail Adresse des Besuchers als Absender. false= Nein ; true = Ja
 $name_von_emailfeld = "Email";   //Feld in der die Absenderadresse steht
  
@@ -8,8 +8,8 @@ $empfaenger = "chris@dailyofthemonth.de"; //Empfänger-Adresse
 $mail_cc = ""; //CC-Adresse, diese E-Mail-Adresse bekommt einer weitere Kopie
 $betreff = "Neue Kontaktanfrage von agile-podcast.de"; //Betreff der Email
  
-$url_ok = "http://www.domain.de/ok.php"; //Zielseite, wenn E-Mail erfolgreich versendet wurde
-$url_fehler = "http://www.domain.de/fehler.php"; //Zielseite, wenn E-Mail nicht gesendet werden konnte
+$url_ok = "https://www.agile-podcast.de/contact/thankyou"; //Zielseite, wenn E-Mail erfolgreich versendet wurde
+$url_fehler = "https://www.agile-podcast.de/contact/error"; //Zielseite, wenn E-Mail nicht gesendet werden konnte
  
  
 //Diese Felder werden nicht in der Mail stehen
